3625ea36ff5bada0228f9820e1677d97a3cb7b94
kamurskol <capic.2021118tads0114@aluno.ifpi.edu.br>
2025-01-08 10:31:06 -0300

    first commit

---
+++ Alexandria.java
@@ -0,0 +1,72 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+//cria a classe que será a biblioteca
+//classe pública permite que outras classes acessem
+public class Alexandria {
+//atributo privado livros que é uma lista de objetos do tipo Livro
+    private List<Livro> livros;
+//atributo do tipo livro que é uma lista de livros emprestados
+    private List<Livro> livrosEmprestados;
+//define o construtor da classe alexandria
+    public Alexandria() {
+        super();
+        this.livros = new ArrayList<>();
+        this.livrosEmprestados = new ArrayList<>();
+
+    }
+//método que cadastra um livro
+//recebe um objeto do tipo Livro como parâmetro
+    public void cadastrarLivro(Livro livro) {
+        livros.add(livro);
+        System.out.println("\nLivro cadastrado com sucesso!");
+
+    } 
+//método que lista os livros
+    public void listarLivros() {
+//percorre a lista de livros
+        for (Livro livro : livros) {
+//imprime o título e o autor do livro
+            System.out.println("Título: " + livro.getTitulo() + ", Autor: " + livro.getAutor());
+
+        }
+
+    }
+//método de emprestar livro
+    public void emprestarLivro(String titulo) {
+//percorre a lista de livros
+        for (Livro livro : livros) {
+//se o título do livro for igual ao título passado como parâmetro
+            if (livro.getTitulo().equals(titulo)) {
+//remove o livro da lista de livros
+                livros.remove(livro);
+//e o adiciona na lista de livros emprestados
+                livrosEmprestados.add(livro);
+                System.out.println("\nLivro emprestado com sucesso!");
+
+                return;
+
+            }else{
+                System.out.println("\nLivro não encontrado");
+            }
+        }
+    }
+//método de devolver livro
+    public void devolverLivro(Livro livro) {
+//se a lista de livros emprestados tiver o livro
+        if (livrosEmprestados.contains(livro)){
+//remove o livro da lista de livros emprestados
+            livrosEmprestados.remove(livro);
+//e adiciona o livro na lista de livros
+            livros.add(livro);
+            System.out.println("Livro devolvido com sucesso!");
+//caso o livro não esteja na lista de emprestados
+        }else{
+//uma mensagem é exibida
+            System.out.println("Livro não encontrado!");
+        }
+
+    }
+
+}
---
+++ App.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+//cria um objeto da classe Alexandria
+        Alexandria biblioteca = new Alexandria();
+//cria um objeto da classe Scanner para ler a entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+//variável que armazena a opção escolhida pelo usuário
+        int opcao;
+//laço de repetição que exibe o menu enquanto o usuário não escolher a opção de sair
+        do {
+//exibe o menu com escolhas de opções
+            System.out.println("\n1. Cadastrar livro");
+            System.out.println("2. Listar livros");
+            System.out.println("3. Emprestar livro");
+            System.out.println("4. Devolver livro");
+            System.out.println("5. Sair\n");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+//switch case que verifica a opção escolhida pelo usuário
+            switch (opcao) {
+//caso a opção seja 1 - cadastrar livro
+                case 1:
+                    System.out.print("Digite o título do livro: ");
+//o usuario digita o título do livro
+                    String titulo = scanner.nextLine();
+                    System.out.print("Digite o autor do livro: ");
+//e o autor
+                    String autor = scanner.nextLine();
+//cria um objeto do tipo livro com o título e autor digitados
+                    biblioteca.cadastrarLivro(new Livro(titulo, autor));
+//executa a função cadastrar livro da classe Alexandria
+//finaliza o case
+                    break;
+
+//caso a opção seja 2 - listar livros
+                case 2:
+//executa a função listar livros da classe Alexandria
+                    biblioteca.listarLivros();
+//finaliza o case
+                    break;
+                case 3:
+//caso a opção seja 3 - emprestar livro
+                    System.out.print("Digite o título do livro para emprestar: \n");
+//o usuário digita o título do livro
+                    String tituloEmprestimo = scanner.nextLine();
+//a função emprestar livro da classe Alexandria é executada
+                    biblioteca.emprestarLivro(tituloEmprestimo);
+//finaliza o case
+                    break;
+
+                case 4:
+//caso a opção seja 4 - devolver livro
+                    System.out.print("Digite o título do livro para devolver: \n");
+//o usuário digita o título do livro
+                    String tituloDevolucao = scanner.nextLine();
+//a função devolver livro da classe Alexandria é executada
+                    biblioteca.devolverLivro(new Livro(tituloDevolucao, ""));
+//finaliza o case
+                    break;
+                case 5:
+//caso o usuário escolha a opção 5 - sair
+                    System.out.println("Saindo...");
+//é exibida uma mensagem de saída e o programa é finalizado
+                    break;
+                default:
+//caso o usuário escolha uma opção diferente das anteriores
+                    System.out.println("Opção inválida!");
+            }
+//o menu será exibido enquanto o usuário não escolher a opção de sair
+        } while (opcao != 5);
+//fecha o scanner
+        scanner.close();
+    }
+}
